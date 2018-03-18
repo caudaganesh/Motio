c2a21195cc273975a33678d8ddc852f17a53439b
caudaganesh <caudaganesh@gmail.com>
2018-03-18 20:13:19 +0700

    create constants sliding up modal menu

---
+++ app/SlidingUpModalMenu/constants.js
@@ -0,0 +1,14 @@
+export const options = {
+    containerOpt: {
+      backgroundColor: 'transparent',
+      padding: 5,
+      borderRadius: 5,
+      width: 220,
+    },
+    text: {
+      fontSize: 30,
+      color: '#000',
+      marginLeft: 7,
+      fontWeight: 'bold'
+    }
+  };
